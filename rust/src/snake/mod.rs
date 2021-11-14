72d0429fa14994b3811f48ae8e6d26b84df158de
Carter_E_Scadden <carter@srtlabs.com>
2021-11-14 11:29:16 -0700

    added onclick, and to added another scope to check if a loop of incrementing clicks works alongside the onclick

--- rust/src/snake/mod.rs
+++ rust/src/snake/mod.rs
@@ -1,4 +1,6 @@
-use wasm_bindgen::JsValue;
+use std::{cell::RefCell, rc::Rc, thread::sleep, time::Duration};
+
+use wasm_bindgen::{prelude::Closure, JsCast, JsValue};
 
 mod init;
 mod state;
@@ -18,96 +20,39 @@ pub fn run(root_id: &str) {
 
             let boxed_ctx = Box::new(init_data.ctx);
 
-            let mut s = state::State::new(boxed_ctx);
+            let state_ref = Rc::new(RefCell::new(state::State::new(boxed_ctx)));
 
-            utils::logger::info(&format!("state has clicks: {}", s.clicks));
+            {
+                let cloned_state = state_ref.clone();
 
-            s.click();
+                let callback = Closure::wrap(Box::new(move |_event: web_sys::Event| {
+                    let mut s = cloned_state.borrow_mut();
+                    s.grow = !s.grow;
+                    s.click();
+                    s.render();
+                }) as Box<dyn FnMut(_)>);
 
-            utils::logger::info(&format!("state has clicks: {}", s.clicks));
+                match init_data
+                    .canvas
+                    .add_event_listener_with_callback("click", callback.as_ref().unchecked_ref())
+                {
+                    Err(e) => utils::logger::error(&format!("Error: {:?}", e)),
+                    Ok(_) => callback.forget(),
+                }
+            }
 
-            utils::logger::info(&format!("state has width: {}", init_data.canvas.width()));
+            {
+                utils::logger::info(&format!("A: {}", state_ref.borrow().clicks));
 
-            s.render();
+                for _ in 0..50 {
+                    let mut s = state_ref.borrow_mut();
+                    utils::logger::info(&format!("Loop: {}", s.clicks));
+                    s.click();
+                    s.render();
+                    // sleep(Duration::from_secs(1));
+                }
+            }
         }
         Err(e) => utils::logger::error(&format!("Error: {:?}", e)),
     }
-    /*
-
-    // Boxes are essentially a cpp unique_pointer,
-    // in other words
-    //
-    // if we were to do this in golang it would be like:
-    // context = *context;
-    // but instead of the pointer being to the place in memory, or stack
-    // the pointer is to a place on the heap
-    //
-    // what this really means is that we can have context shared across multiple stacks
-    let context = Box::new(context);
-
-    {
-        // clone the pointer, ie, let us us context on this stack
-        let context = context.clone();
-
-        // Rc is a reference kinda like &variable,
-        // but in this case where only one thing can have access to a variable at a time, Rc allows
-        // a value to be "shared" in this case, it kinda like a shared reference
-        //
-        // RefCell makes a value dynamically mutable
-        //
-        // tldr: this is saying, state is a something that is going to be shared, and its contents
-        // are mutable for everyone
-        //
-        // https://stackoverflow.com/questions/61997859/understanding-usage-of-rcrefcellsomestruct-in-rust
-        let state = Rc::new(RefCell::new(state::State::new(context)));
-
-        {
-            // TODO: figure out how to create a wrap function ie:
-            // ```
-            // function wrap(a, b) {
-            //  const value = a();
-            //  b();
-            //  return value;
-            // }
-            // ```
-            //
-            // this would simplify a lot of out code, because we could just wrap a: render() in it,
-            // or if we want, we can somehow use some sort of observable / value that we can set,
-            // ie:
-            // state::State::new(context, render)
-
-            // TODO: figure out how to wrap this up
-            // something like
-            // ```
-            // let callback = utils::use_closure<Event>(func: Box<dyn FnMut(_)>)
-            // let forget = utils::use_event(element: &?, event_name: &str, callback: &?);
-            // ```
-            // ^ if something like the above is possible at all
-            let state = state.clone();
-
-            // this closure and all of this i dont quite understand too well, but im sure we will
-            // figure it out
-            let callback = Closure::wrap(Box::new(move |_event: web_sys::Event| {
-                let mut s = state.borrow_mut();
-                s.click();
-                s.render();
-                log("clicked!!!!");
-            }) as Box<dyn FnMut(_)>);
-
-            canvas.add_event_listener_with_callback("click", callback.as_ref().unchecked_ref())?;
-
-            // this hands memory management of the callback to javascript
-            //
-            // ie, because we have attached the callback to the html dom element, javascript now
-            // has control of the element and the callback, because of this, we tell wasm to not
-            // bother freeing the callback memory, but to just let javascript do it.
-            //
-            // see
-            // callback.into_js_value();
-            callback.forget();
-        }
-    }
-
-    Ok(())
-    */
 }
--- rust/src/snake/state/mod.rs
+++ rust/src/snake/state/mod.rs
@@ -3,6 +3,7 @@ use web_sys::CanvasRenderingContext2d;
 
 pub struct State {
     pub clicks: i32,
+    pub grow: bool,
     aspect: i32,
     pub ctx: Box<CanvasRenderingContext2d>,
 }
@@ -12,6 +13,7 @@ impl State {
         State {
             clicks: 0,
             aspect: 0,
+            grow: true,
             ctx,
         }
     }
@@ -19,7 +21,8 @@ impl State {
     pub fn render(&mut self) {
         let ctx = self.ctx.as_mut();
         ctx.set_fill_style(&JsValue::from_str("red"));
-        ctx.fill_rect(10.0, 10.0, 100.0, 100.0);
+        let offset = self.clicks as f64;
+        ctx.fill_rect(10.0 + offset, 10.0 + offset, 100.0, 100.0);
     }
 
     pub fn click(&mut self) {
