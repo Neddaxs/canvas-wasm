340f1f45c3a1e0aa63255f8a92ed756713c6c444
Tanner Scadden <tanner@scaddenfamily.com>
2021-11-15 16:05:38 -0700

    Reorganized event handlers

--- rust/src/snake/mod.rs
+++ rust/src/snake/mod.rs
@@ -2,6 +2,7 @@ use std::{cell::RefCell, rc::Rc, thread::sleep, time::Duration};
 
 use wasm_bindgen::{prelude::Closure, JsCast};
 
+mod events;
 mod game_state;
 mod init;
 mod state;
@@ -46,48 +47,10 @@ pub fn run(root_id: &str) {
             let boxed_ctx = Box::new(init_data.ctx);
             let state_ref = Rc::new(RefCell::new(state::State::new(boxed_ctx)));
 
-            // KeyDown
-            {
-                let cloned_state_ref = state_ref.clone();
+            let boxed_canvas = Box::new(init_data.canvas);
+            let canvas_ref = Rc::new(RefCell::new(boxed_canvas));
 
-                let key_press_callback =
-                    Closure::wrap(Box::new(move |event: web_sys::KeyboardEvent| {
-                        utils::logger::info("keydown");
-                        event.prevent_default();
-
-                        let mut s = cloned_state_ref.borrow_mut();
-                        let key = utils::keys::get_key(event.key().as_str());
-                        utils::logger::info(&format!("{:?}", key));
-                        s.change_direction(key);
-                    }) as Box<dyn FnMut(_)>);
-
-                match init_data.window.add_event_listener_with_callback(
-                    "keydown",
-                    key_press_callback.as_ref().unchecked_ref(),
-                ) {
-                    Err(e) => utils::logger::error(&format!("Error: {:?}", e)),
-                    Ok(_) => key_press_callback.forget(),
-                }
-            }
-
-            // OnClick
-            {
-                let cloned_state = state_ref.clone();
-
-                let onclick_callback = Closure::wrap(Box::new(move |_event: web_sys::Event| {
-                    utils::logger::info("click");
-                    let mut s = cloned_state.borrow_mut();
-                    s.toggle_game();
-                }) as Box<dyn FnMut(_)>);
-
-                match init_data.canvas.add_event_listener_with_callback(
-                    "click",
-                    onclick_callback.as_ref().unchecked_ref(),
-                ) {
-                    Err(e) => utils::logger::error(&format!("Error: {:?}", e)),
-                    Ok(_) => onclick_callback.forget(),
-                }
-            }
+            events::listeners::register(&canvas_ref, &state_ref);
 
             // Renderer
             {
